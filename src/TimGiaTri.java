8e87811ca2e214d36d2f90270a6e18b0f7f56abf
sonnguyen610 <sonnguyen6103@gmail.com>
2018-06-19 15:47:42 +0700

    Tim gia tri trong mang

--- src/TimGiaTri.java
+++ src/TimGiaTri.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TimGiaTriTrongMang {
+public class TimGiaTri {
     public static void main(String[] args) {
         String[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
         Scanner scanner = new Scanner(System.in);
