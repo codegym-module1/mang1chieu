8345d7eb337c146dbdf33dc4d892a9dbab8d6692
sonnguyen610 <40166074+sonnguyen610@users.noreply.github.com>
2018-06-21 21:29:02 +0700

    Delete n.java

--- src/n.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class n {
-    public static void main(String[] args) {
-        int[] nubers = {2, 3, 5, 7};
-        for (int i = 0; i < nubers.length; i++) {
-            System.out.print(nubers[i] + "\t");
-        }
-    }
-}
